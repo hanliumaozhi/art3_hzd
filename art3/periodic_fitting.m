275c321a56c12b61b21307af11e1315c8ce023ee
song liu <liusong.me@gmail.com>
2018-08-10 21:50:05 +0800

    fix #5, see #3

--- art3/periodic_fitting.m
+++ art3/periodic_fitting.m
@@ -1,6 +1,6 @@
 %% Generate best fit function
 target_vy = 0.0;
-target_vx = [-0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4];
+target_vx = [-0.8, -0.6, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.8];
 T = 0.4;
 subfolder_name = 'library';
 if ~exist(fullfile('local', subfolder_name, 'periodic_fitting'), 'dir')
@@ -31,9 +31,7 @@ t_all = cell(N_vx, N_vy);
 q_all = cell(N_vx, N_vy);
 dq_all = cell(N_vx, N_vy);
 px_all = cell(N_vx, N_vy);
-py_all = cell(N_vx, N_vy);
 vx_all = cell(N_vx, N_vy);
-vy_all = cell(N_vx, N_vy);
 counter = 1;
 for i = 1:N_vx
     vx = target_vx(i);
@@ -45,21 +43,14 @@ for i = 1:N_vx
         t_all{i,j} = [param.gait(1).tspan]./T;%, param.gait(3).tspan];
         q_all{i,j} = [param.gait(1).states.x];%, param. gait(3).states.x];
         dq_all{i,j} = [param.gait(1).states.dx];%, param. gait(3).states.dx];
-        px_all{i,j} = [param.gait(1).states.x(1,:)];%ones(size(t_all{i,j}))*vx;
-        py_all{i,j} = [param.gait(1).states.x(2,:)];%ones(size(t_all{i,j}))*vy;
-        vx_all{i,j} = [param.gait(1).states.dx(1,:)];%ones(size(t_all{i,j}))*vx;
-        vy_all{i,j} = [param.gait(1).states.dx(2,:)];%ones(size(t_all{i,j}))*vy;     
+        vx_all{i,j} = ones(size(t_all{i,j}))*vx;%ones(size(t_all{i,j}))*vx;
     end
 end
 
-inpt = [horzcat(t_all{:})
-%     horzcat(px_all{:})
-%     horzcat(py_all{:})
-    horzcat(vx_all{:})
-%     horzcat(vx_all{:})
-    ];
+inpt = [horzcat(t_all{:}); horzcat(vx_all{:})];
 q_outpt = horzcat(q_all{:});
 dq_outpt = horzcat(dq_all{:});
+
 %%
 for i = 1:N
     q_outpt_specific = q_outpt(i, :);
@@ -69,7 +60,6 @@ for i = 1:N
 end
 addpath(fullfile('local', subfolder_name, 'periodic_fitting'));
 %% Plot features
-% joint angle
 for n = 1:N
     f = figure(1000+n);
     f.Name = joint_names{n};
